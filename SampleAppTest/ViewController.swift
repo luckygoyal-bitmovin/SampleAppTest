ed51c15f9cc7bddad111ea38e31095f932690642
Lucky Goyal <lucky.goyal@bitmovin.com>
2023-07-12 15:38:04 +0200

    Test app for SPM test

---
+++ SampleAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAppTest
+//
+//  Created by Karthi on 27/06/23.
+//
+
+import UIKit
+//import MKPlayer
+import AdvancedFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       let _ = PlayerWrapper()
+    }
+}
+
